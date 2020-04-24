314f6dcb57fe1f1df9db899fa18faf4e81b29b58
Shubhadeep394 <shubhadeepmandal394@gmail.com>
2020-04-24 21:26:08 +0530

    added function block to read sensor value

--- telegram_alert.py
+++ telegram_alert.py
@@ -7,3 +7,18 @@ import conf                     # config file
 
 mybolt = Bolt(conf.bolt_api_key, conf.device_id)
 
+def get_sensor_value_from_pin(pin):
+    """Returns the sensor value. Returns -999 if request fails"""
+    try:
+        response = mybolt.analogRead(pin)
+        data = json.loads(response)
+        if data["success"] != 1:
+            print("Request not successfull")
+            print("This is the response->", data)
+            return -999
+        sensor_value = int(data["value"])
+        return sensor_value
+    except Exception as e:
+        print("Something went wrong when returning the sensor value")
+        print(e)
+        return -999
