1f1c4ed893d561b6ae4181bf344e244aae22ccf3
Shubhadeep394 <shubhadeepmandal394@gmail.com>
2020-04-24 21:37:00 +0530

    added function block to send messages

--- telegram_alert.py
+++ telegram_alert.py
@@ -22,3 +22,47 @@ def get_sensor_value_from_pin(pin):
         print("Something went wrong when returning the sensor value")
         print(e)
         return -999
+def send_telegram_message(message):
+    """Sends message via Telegram"""
+    url = "https://api.telegram.org/" + conf.telegram_bot_id + "/sendMessage"
+    data = {
+        "chat_id": conf.telegram_chat_id,
+        "text": message
+    }
+    try:
+        response = requests.request(
+            "POST",
+            url,
+            params=data
+        )
+        print("This is the Telegram URL")
+        print(url)
+        print("This is the Telegram response")
+        print(response.text)
+        telegram_data = json.loads(response.text)
+        return telegram_data["ok"]
+    except Exception as e:
+        print("An error occurred in sending the alert message via Telegram")
+        print(e)
+        return False
+while True:
+    # Step 1
+    sensor_value = get_sensor_value_from_pin("A0")    
+    print("The current sensor value is:", sensor_value)
+    
+    # Step 2
+    if sensor_value == -999:
+        print("Request was unsuccessfull. Skipping.")
+        time.sleep(10)
+        continue
+    
+    # Step 3
+    if sensor_value >= conf.threshold:
+        print("Sensor value has exceeded threshold")
+        message = "Alert! Sensor value has exceeded " + str(conf.threshold) + \
+                  ". The current value is " + str(sensor_value)
+        telegram_status = send_telegram_message(message)
+        print("This is the Telegram status:", telegram_status)
+
+    # Step 4
+    time.sleep(10)
