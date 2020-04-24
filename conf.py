90a3afe3152d0653d846d45d52f98072026a4275
Shubhadeep394 <shubhadeepmandal394@gmail.com>
2020-04-24 22:13:15 +0530

    Final Commit

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 """Configurations for telegram_alert.py"""
 bolt_api_key = "1d717aa3-939a-470c-bddb-0d01902617e7" # This is your Bolt Cloud API Key
 device_id = "BOLT11719328" # This is the device ID and will be similar to BOLTXXXX where XXXX is some numbers
-telegram_chat_id = "@shubhadeep_temperature_alert" # This is the channel ID of the created Telegram channel.Paste after @
-telegram_bot_id = "bot1095211901:AAEDOpa5RtZgV0DrF2nL7y-PS0YZX8Lqz8g" # This is the bot ID of the created Telegram Bot. Pasteafter bot
+telegram_chat_id = "@shubhadeep_temperature_alert" # This is the channel ID of the created Telegram channel.Paste after '@'
+telegram_bot_id = "bot1095211901:AAEDOpa5RtZgV0DrF2nL7y-PS0YZX8Lqz8g" # This is the bot ID of the created Telegram Bot. Paste after 'bot'
 threshold = 250 # Threshold beyond which the alert should be sent
